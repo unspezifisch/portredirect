902b6f710ccf1e5dcaba97e999fa4ced626595ee
fnoord <fnoord@example.com>
2024-12-08 02:08:32 +0100

    change handlers

--- src/quic.rs
+++ src/quic.rs
@@ -1,96 +1,100 @@
 // from https://github.com/quinn-rs/quinn/blob/204b14792b5e92eb2c43cdb1ff05426412ff4466/quinn/examples/server.rs
 use std::{
-    ascii, fs, io,
-    path::{self, Path, PathBuf},
-    str,
-    sync::Arc,
+    ascii, fs, io, net::SocketAddr, path::{self, Path, PathBuf}, str, sync::Arc
 };
 
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Error, Result};
 use quinn::crypto::rustls::QuicServerConfig;
 use rustls::pki_types::{CertificateDer, PrivateKeyDer, PrivatePkcs8KeyDer};
 
-#[tokio::main]
-async fn setup_quic(options: Opt) -> Result<()> {
-    let (certs, key) = if let (Some(key_path), Some(cert_path)) = (&options.key, &options.cert) {
-        let key = fs::read(key_path).context("failed to read private key")?;
-        let key = if key_path.extension().is_some_and(|x| x == "der") {
-            PrivateKeyDer::Pkcs8(PrivatePkcs8KeyDer::from(key))
-        } else {
-            rustls_pemfile::private_key(&mut &*key)
-                .context("malformed PKCS #1 private key")?
-                .ok_or_else(|| anyhow::Error::msg("no private keys found"))?
-        };
-        let cert_chain = fs::read(cert_path).context("failed to read certificate chain")?;
-        let cert_chain = if cert_path.extension().is_some_and(|x| x == "der") {
-            vec![CertificateDer::from(cert_chain)]
-        } else {
-            rustls_pemfile::certs(&mut &*cert_chain)
-                .collect::<Result<_, _>>()
-                .context("invalid PEM-encoded certificate")?
-        };
+#[derive(Debug)]
+struct QuicConfig {
+    cert_hostname: String,
+    cert_file: PathBuf,
+    key_file: PathBuf,
 
-        (cert_chain, key)
+    listen: SocketAddr,
+    stateless_retry: bool,
+    connection_limit: Option<usize>,
+}
+
+fn try_load_quic_cert(key_path: PathBuf, cert_path: PathBuf) -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>)> {
+    let key = fs::read(key_path).context("failed to read private key")?;
+    let key = if key_path.extension().is_some_and(|x| x == "der") {
+        PrivateKeyDer::Pkcs8(PrivatePkcs8KeyDer::from(key))
     } else {
-        let dirs = directories_next::ProjectDirs::from("org", "quinn", "quinn-examples").unwrap();
-        let path = dirs.data_local_dir();
-        let cert_path = path.join("cert.der");
-        let key_path = path.join("key.der");
-        let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path)?))) {
-            Ok((cert, key)) => (
-                CertificateDer::from(cert),
-                PrivateKeyDer::try_from(key).map_err(anyhow::Error::msg)?,
-            ),
-            Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
-                println!("generating self-signed certificate");
-                let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
-                let key = PrivatePkcs8KeyDer::from(cert.key_pair.serialize_der());
-                let cert = cert.cert.into();
-                fs::create_dir_all(path).context("failed to create certificate directory")?;
-                fs::write(&cert_path, &cert).context("failed to write certificate")?;
-                fs::write(&key_path, key.secret_pkcs8_der())
-                    .context("failed to write private key")?;
-                (cert, key.into())
-            }
-            Err(e) => {
-                bail!("failed to read certificate: {}", e);
-            }
-        };
+        rustls_pemfile::private_key(&mut &*key)
+            .context("malformed PKCS #1 private key")?
+            .ok_or_else(|| anyhow::Error::msg("no private keys found"))?
+    };
+    let cert_chain = fs::read(cert_path).context("failed to read certificate chain")?;
+    let cert_chain = if cert_path.extension().is_some_and(|x| x == "der") {
+        vec![CertificateDer::from(cert_chain)]
+    } else {
+        rustls_pemfile::certs(&mut &*cert_chain)
+            .collect::<Result<_, _>>()
+            .context("invalid PEM-encoded certificate")?
+    };
 
-        (vec![cert], key)
+    Ok((cert_chain, key))
+}
+
+fn generate_quic_cert(key_path: PathBuf, cert_path: PathBuf) -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>), Error> {
+    let (cert, key) = match fs::read(&cert_path).and_then(|x| Ok((x, fs::read(&key_path)?))) {
+        Ok((cert, key)) => (
+            CertificateDer::from(cert),
+            PrivateKeyDer::try_from(key).map_err(anyhow::Error::msg)?,
+        ),
+        Err(ref e) if e.kind() == io::ErrorKind::NotFound => {
+            println!("generating self-signed certificate");
+            let cert = rcgen::generate_simple_self_signed(vec!["localhost".into()]).unwrap();
+            let key = PrivatePkcs8KeyDer::from(cert.key_pair.serialize_der());
+            let cert = cert.cert.into();
+            fs::write(&cert_path, &cert).context("failed to write certificate")?;
+            fs::write(&key_path, key.secret_pkcs8_der())
+                .context("failed to write private key")?;
+            (cert, key.into())
+        }
+        Err(e) => {
+            bail!("failed to read certificate: {}", e);
+        }
+    };
+
+    Ok((vec![cert], key))
+}
+
+#[tokio::main]
+async fn setup_quic(config: QuicConfig) -> Result<()> {
+    let (certs, key) = match try_load_quic_cert(config.key_file, config.cert_file) {
+        Ok(ret) => ret,
+        Err(_) => generate_quic_cert(config.key_file, config.cert_file).context("generating QUIC certificate")?,
     };
 
     let mut server_crypto = rustls::ServerConfig::builder()
         .with_no_client_auth()
         .with_single_cert(certs, key)?;
-    if options.keylog {
-        server_crypto.key_log = Arc::new(rustls::KeyLogFile::new());
-    }
 
     let mut server_config =
         quinn::ServerConfig::with_crypto(Arc::new(QuicServerConfig::try_from(server_crypto)?));
     let transport_config = Arc::get_mut(&mut server_config.transport).unwrap();
     transport_config.max_concurrent_uni_streams(0_u8.into());
 
-    let endpoint = quinn::Endpoint::server(server_config, options.listen)?;
+    let endpoint = quinn::Endpoint::server(server_config, config.listen)?;
     eprintln!("listening on {}", endpoint.local_addr()?);
 
     while let Some(conn) = endpoint.accept().await {
-        if options
+        if config
             .connection_limit
             .is_some_and(|n| endpoint.open_connections() >= n)
         {
             println!("refusing due to open connection limit");
             conn.refuse();
-        } else if Some(conn.remote_address()) == options.block {
-            println!("refusing blocked client IP address");
-            conn.refuse();
-        } else if options.stateless_retry && !conn.remote_address_validated() {
+        } else if config.stateless_retry && !conn.remote_address_validated() {
             println!("requiring connection to validate its address");
             conn.retry().unwrap();
         } else {
             println!("accepting connection");
-            let fut = handle_connection(0, conn);
+            let fut = handle_connection_quic(conn);
             tokio::spawn(async move {
                 if let Err(e) = fut.await {
                     eprintln!("connection failed: {reason}", reason = e.to_string())
@@ -102,27 +106,17 @@ async fn setup_quic(options: Opt) -> Result<()> {
     Ok(())
 }
 
-async fn handle_connection(root: Arc<Path>, conn: quinn::Incoming) -> Result<()> {
+async fn handle_connection_quic(conn: quinn::Incoming) -> Result<()> {
     let connection = conn.await?;
-    let span = info_span!(
-        "connection",
-        remote = %connection.remote_address(),
-        protocol = %connection
-            .handshake_data()
-            .unwrap()
-            .downcast::<quinn::crypto::rustls::HandshakeData>().unwrap()
-            .protocol
-            .map_or_else(|| "<none>".into(), |x| String::from_utf8_lossy(&x).into_owned())
-    );
     async {
-        println!("established");
+        println!("QUIC connection established");
 
         // Each stream initiated by the client constitutes a new request.
         loop {
             let stream = connection.accept_bi().await;
             let stream = match stream {
                 Err(quinn::ConnectionError::ApplicationClosed { .. }) => {
-                    println!("connection closed");
+                    println!("QUIC connection closed");
                     return Ok(());
                 }
                 Err(e) => {
@@ -130,24 +124,21 @@ async fn handle_connection(root: Arc<Path>, conn: quinn::Incoming) -> Result<()>
                 }
                 Ok(s) => s,
             };
-            let fut = handle_request(root.clone(), stream);
+            let fut = handle_request_quic(stream);
             tokio::spawn(
                 async move {
                     if let Err(e) = fut.await {
                         eprintln!("failed: {reason}", reason = e.to_string());
                     }
                 }
-                .instrument(info_span!("request")),
             );
         }
     }
-    .instrument(span)
     .await?;
     Ok(())
 }
 
-async fn handle_request(
-    root: Arc<Path>,
+async fn handle_request_quic(
     (mut send, mut recv): (quinn::SendStream, quinn::RecvStream),
 ) -> Result<()> {
     let req = recv
@@ -159,12 +150,10 @@ async fn handle_request(
         let part = ascii::escape_default(x).collect::<Vec<_>>();
         escaped.push_str(str::from_utf8(&part).unwrap());
     }
-    println!(content = %escaped);
+    println!("{}", escaped);
+
     // Execute the request
-    let resp = process_get(&root, &req).unwrap_or_else(|e| {
-        eprintln!("failed: {}", e);
-        format!("failed to process request: {e}\n").into_bytes()
-    });
+    let resp = vec![0x41, 0x42, 0x43];
     // Write the response
     send.write_all(&resp)
         .await
@@ -173,37 +162,4 @@ async fn handle_request(
     send.finish().unwrap();
     println!("complete");
     Ok(())
-}
-
-fn process_get(root: &Path, x: &[u8]) -> Result<Vec<u8>> {
-    if x.len() < 4 || &x[0..4] != b"GET " {
-        bail!("missing GET");
-    }
-    if x[4..].len() < 2 || &x[x.len() - 2..] != b"\r\n" {
-        bail!("missing \\r\\n");
-    }
-    let x = &x[4..x.len() - 2];
-    let end = x.iter().position(|&c| c == b' ').unwrap_or(x.len());
-    let path = str::from_utf8(&x[..end]).context("path is malformed UTF-8")?;
-    let path = Path::new(&path);
-    let mut real_path = PathBuf::from(root);
-    let mut components = path.components();
-    match components.next() {
-        Some(path::Component::RootDir) => {}
-        _ => {
-            bail!("path must be absolute");
-        }
-    }
-    for c in components {
-        match c {
-            path::Component::Normal(x) => {
-                real_path.push(x);
-            }
-            x => {
-                bail!("illegal component in path: {:?}", x);
-            }
-        }
-    }
-    let data = fs::read(&real_path).context("failed reading file")?;
-    Ok(data)
 }

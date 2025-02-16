0a663b022cc5a1fb56de765ee0060e9e4a146b9f
fnoord <fnoord@example.com>
2025-02-16 01:55:46 +0100

    try round bird

--- src/quic/mod.rs
+++ src/quic/mod.rs
@@ -13,7 +13,8 @@ pub const ALPN_QUIC_PORTREDIRECT: &[&[u8]] = &[b"pr-1"];
 pub fn configure_transport_config(transport_config: &mut TransportConfig) {
     // QUIC connection advanced configuration
     
-    transport_config.send_fairness(false);
+    // Schedule streams in a round-robin fashion
+    transport_config.send_fairness(true);
 
     // We manage our own connection limit
     transport_config.max_concurrent_uni_streams(0_u8.into());
@@ -22,7 +23,7 @@ pub fn configure_transport_config(transport_config: &mut TransportConfig) {
     // 30s timeout is QUIC's default timeout
     transport_config.keep_alive_interval(Some(std::time::Duration::from_secs(25)));
 
-    //transport_config.crypto_buffer_size(crate::PortRedirectProtocol::QUIC_CRYPTO_BUFFER_SIZE);
+    transport_config.crypto_buffer_size(crate::PortRedirectProtocol::QUIC_CRYPTO_BUFFER_SIZE);
 
     // We don't want to "sacrifice privacy" to more easily measure latency
     transport_config.allow_spin(false);
